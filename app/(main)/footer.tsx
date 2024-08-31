34c558ab10962e3ee06cb01e96eb54986bb6e5f9
zhojielun <137139821+zhojielun@users.noreply.github.com>
2024-08-31 12:00:04 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -19,40 +19,57 @@ export default function Footer() {
           </section>
           <section className="mt-4">
             <p className="text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-              Friendly Links:
+              Friendly Links:{" "}
+              <a
+                href="https://enblog.top"
+                target="_blank"
+                className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+              >
+                个人博客
+              </a>
+              ,{" "}
+              <a
+                href="https://panel.vvv.vg"
+                target="_blank"
+                className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+              >
+                机场
+              </a>
+              ,{" "}
+              <a
+                href="https://nezha.uk"
+                target="_blank"
+                className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+              >
+                哪吒探针
+              </a>
             </p>
-            <ul className="mt-2 flex flex-col gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-              <li>
-                <a
-                  href="https://enblog.top"
-                  target="_blank"
-                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
-                >
-                  个人博客
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://panel.vvv.vg"
-                  target="_blank"
-                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
-                >
-                  机场
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://nezha.uk"
-                  target="_blank"
-                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
-                >
-                  哪吒探针
-                </a>
-              </li>
-            </ul>
           </section>
         </section>
       </footer>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            function fetchData() {
+              fetch('https://nezha.uk/ipinfo.php')
+                .then(response => response.json())
+                .then(data => {
+                  document.getElementById('cf-country').textContent = data.cf_node_country;
+                  document.getElementById('cf-city').textContent = data.cf_node_city;
+                  document.getElementById('cf-ip').textContent = data.cf_node;
+                  document.getElementById('cf-flag').src = \`https://raw.githubusercontent.com/hampusborgos/country-flags/main/svg/\${data.cf_node_country_code}.svg\`;
+
+                  document.getElementById('user-country').textContent = data.user_country;
+                  document.getElementById('user-city').textContent = data.user_city;
+                  document.getElementById('user-ip').textContent = data.user_ip;
+                  document.getElementById('user-flag').src = \`https://raw.githubusercontent.com/hampusborgos/country-flags/main/svg/\${data.user_country_code}.svg\`;
+                });
+            }
+
+            fetchData();
+          `,
+        }}
+      />
       <script
         defer
         src="https://umami.analyze.ng/script.js"
