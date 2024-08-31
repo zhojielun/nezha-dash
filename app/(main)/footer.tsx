246f2684800bb157b2b03162ca7a432352a17ddb
zhojielun <137139821+zhojielun@users.noreply.github.com>
2024-08-31 11:54:58 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -17,6 +17,40 @@ export default function Footer() {
             © 2020-{new Date().getFullYear()}{" "}
             <a href={"https://github.com/zhojielun"}>@zhojielun</a>
           </section>
+          <section className="mt-4">
+            <p className="text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+              Friendly Links:
+            </p>
+            <ul className="mt-2 flex flex-col gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+              <li>
+                <a
+                  href="https://enblog.top"
+                  target="_blank"
+                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+                >
+                  个人博客
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://panel.vvv.vg"
+                  target="_blank"
+                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+                >
+                  机场
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://nezha.uk"
+                  target="_blank"
+                  className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
+                >
+                  哪吒探针
+                </a>
+              </li>
+            </ul>
+          </section>
         </section>
       </footer>
       <script
